84c2d47e04ccebf95deb51a3c5d2f45bdaf394ae
Gonzalo Esteban Ramos Zuniga <einfari@MacBook-Pro.local>
2023-03-23 08:51:54 -0400

    refactor: architecting certification

--- src/components/Certifications/badges.js
+++ src/components/Certifications/badges.js
@@ -1,18 +1,25 @@
 import CCP_BADGE_IMG from '../../img/aws-certified-cloud-practitioner.png';
 import CDA_BADGE_IMG from '../../img/aws-certified-developer-associate.png';
+import ARCH_BADGE_IMG from '../../img/aws-learning-architecting.png';
 
 const badges = [
   {
     url: 'https://www.credly.com/badges/7a0cf1bb-f168-4dc1-97bf-fc96e806356e/' +
       'public_url',
     img: CCP_BADGE_IMG,
-    alt: 'CCP Badge'
+    alt: 'AWS Certified Cloud Practitioner Badge'
   },
   {
     url: 'https://www.credly.com/badges/cc08080d-a727-496b-883a-472f74b44403/' +
       'public_url',
     img: CDA_BADGE_IMG,
-    alt: 'CDA BADGE'
+    alt: 'AWS Certified Developer Associate Badge'
+  },
+  {
+    url: 'https://www.credly.com/badges/44966e46-d68e-4d22-b041-2ed116544a81/' +
+      'public_url',
+    img: ARCH_BADGE_IMG,
+    alt: 'AWS Learning: Architecting Badge'
   }
 ]
 
